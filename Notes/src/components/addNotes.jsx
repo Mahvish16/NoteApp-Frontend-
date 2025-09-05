938bd9672e595b6cc970471cf7543a1b7c36f47a
Mahvish16 <mahvish.ruhi@gmail.com>
2025-09-05 22:42:14 +0530

    Addnotes frontend

---
+++ Notes/src/components/addNotes.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNote } from "../actions/addApi";
+import './Add.css';
+
+function Add() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const token = useSelector((state) => state.login.access);
+
+
+    if (!token) {
+        alert("User is not logged in.");
+        return;
+    }
+
+
+    const dispatch = useDispatch();
+
+    const handleAddNote = (e) => {
+        e.preventDefault();
+        if (!title || !description) {
+            alert("Both title and description are required.");
+            return;
+        }
+        else {
+            alert("Successfully added")
+        }
+        const noteData = {
+            title: title,
+            description: description,
+        };
+
+        dispatch(addNote(token, noteData));
+        setTitle("");
+        setDescription("");
+    };
+
+    return (
+        <div className="full-page-note">
+            <form onSubmit={handleAddNote} className="note-form">
+                <h2>Add Your Note</h2>
+
+                <input
+                    type="text"
+                    placeholder="Enter note title..."
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <textarea
+                    placeholder="Enter note description..."
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+
+                <button type="submit">Save Note</button>
+            </form>
+
+        </div>
+    );
+}
+
+export default Add;
